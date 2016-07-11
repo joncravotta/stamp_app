1f1744892d1bde6206e3cdb5ef15c90d8034fd55
joncravotta <joncravotta32@gmail.com>
2016-07-10 22:23:54 -0400

    messing up schema

--- db/migrate/20160621183215_change_data_type__user.rb
+++
@@ -1,6 +0,0 @@
-class ChangeDataTypeUser < ActiveRecord::Migration
-  def change
-    change_column :users, :stripe_customer_id, :string, default: nil
-    change_column :users, :stripe_plan_id, :string, default: nil
-  end
-end
--- db/migrate/20160621184146_change_date_type__user.rb
+++
@@ -1,7 +0,0 @@
-class ChangeDateTypeUser < ActiveRecord::Migration
-  def change
-    change_column :users, :stripe_current_period_start, :integer, default: nil
-    change_column :users, :stripe_current_period_end, :integer, default: nil
-    change_column :users, :stripe_canceled_at, :integer, default: nil
-  end
-end

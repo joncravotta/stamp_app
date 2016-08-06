d53b7dcc1a21ba803aacc657edaafd4ba320e7a2
joncravotta <joncravotta32@gmail.com>
2016-08-06 18:05:29 -0400

    day two

--- lib/slice_model.rb
+++ lib/slice_model.rb
@@ -1,8 +1,8 @@
 class SliceModel
   def initialize(data)
     @data = data["slice_data"]
-    @image_width = data["imageWidth"]
-    @image_height = data["imageHeight"]
+    @image_width = data["imageWidth"].to_i
+    @image_height = data["imageHeight"].to_i
     @formatted_data = []
     @horizontal = []
     @vertical = []
@@ -49,45 +49,49 @@ class SliceModel
       puts "context vert sorted #{sorted_verts}"
 
       context_count = 0
-      context_length = context_verticals.length
+      context_length = context_verticals.length - 1
+      previous_vertical = {}
 
       context_verticals.each do |k, v|
-        previous_vertical = {}
         if context_count == 0
+          puts "first block, context_count: #{context_count}, previous vertical: #{previous_vertical}}"
           hash = {}
           hash[:x] = last_horizontal["startX"]
           hash[:y] = last_horizontal["startY"]
-          hash[:width] = v["startX"] - last_horizontal["startX"]
-          hash[:height] = v["endY"] - last_horizontal["startY"]
-          previous_vertical[:x] = last_horizontal["startX"]
-          previous_vertical[:y] = last_horizontal["startY"]
+          hash[:width] = k["startX"].to_i - last_horizontal["startX"].to_i
+          hash[:height] = k["endY"].to_i - last_horizontal["startY"].to_i
+          previous_vertical[:x] = k["startX"]
+          previous_vertical[:y] = k["startY"]
           @formatted_data.push(hash)
         # this is how we know its the last vertical for this horizontals
         elsif context_count == context_length
+          puts "second block, context_count: #{context_count}, previous vertical: #{previous_vertical}}"
           hash = {}
-          hash[:x] = previous_vertical["x"]
-          hash[:y] = previous_vertical["y"]
-          hash[:width] = v["startX"] - previous_vertical["x"]
-          hash[:height] = v["endY"] - previous_vertical["y"]
+          hash[:x] = previous_vertical["x"].to_i
+          hash[:y] = previous_vertical["y"].to_i
+          hash[:width] = k["startX"].to_i - previous_vertical["x"].to_i
+          hash[:height] = k["endY"].to_i - previous_vertical["y"].to_i
           @formatted_data.push(hash)
           last_hash = {}
-          hash[:x] = v["startX"]
-          hash[:y] = v["startY"]
-          hash[:width] = imageWidth - v["startX"]
-          hash[:height] = v["endY"] - last_horizontal["y"]
+          hash[:x] = k["startX"].to_i
+          hash[:y] = k["startY"].to_i
+          hash[:width] = @image_width - k["startX"].to_i
+          hash[:height] = k["endY"].to_i - last_horizontal["y"].to_i
           @formatted_data.push(hash)
         # this is a vertical in between other verticals
         else
+          puts "third block, context_count: #{context_count}, previous vertical: #{previous_vertical}}"
           hash = {}
-          hash[:x] = previous_vertical["x"]
-          hash[:y] = previous_vertical["y"]
-          hash[:width] = v["startX"] - previous_vertical["x"]
-          hash[:height] = v["endY"] - previous_vertical["y"]
-          previous_vertical[:x] = v["startX"]
-          previous_vertical[:y] = v["startY"]
+          hash[:x] = previous_vertical["x"].to_i
+          hash[:y] = previous_vertical["y"].to_i
+          hash[:width] = k["startX"].to_i - previous_vertical["x"].to_i
+          hash[:height] = k["endY"].to_i - previous_vertical["y"].to_i
+          previous_vertical[:x] = k["startX"]
+          previous_vertical[:y] = k["startY"]
           @formatted_data.push(hash)
         end
-
+        context_count  = context_count + 1
+        puts @formatted_data
       end
     end
   end

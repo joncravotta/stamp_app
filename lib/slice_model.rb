9b796e027da3ed1d2b4dabcee1edc9346930ca1f
joncravotta <joncravotta32@gmail.com>
2016-12-03 12:56:11 -0500

    mo

--- lib/slice_model.rb
+++ lib/slice_model.rb
@@ -1,5 +1,6 @@
 require "byebug"
 class SliceModel
+
   CropHash = Struct.new(:x, :y, :width, :height)
 
   def initialize(data)
@@ -97,60 +98,6 @@ class SliceModel
 
   def sort_verticals(unsorted_verticals)
     unsorted_verticals.sort_by! { |k| k[:startX].to_i }
-  end
-
-  def create_crops
-    #PERVIOUS horizontal line x,y is stored here
-    prev_horizontal_y = 0
-    #looping through each sorted horizontal line
-    @horizontal.each do |key, value|
-      # getting all verticals that match up with current horizontal
-      # CONTEXT VERTICALS ARE VERTICAL THAT ARE IN CURRENT HORIZONTAL
-      current_horizontal_y = key["startY"]
-      context_verticals = []
-      @vertical.each do |k, v|
-        current_vertical_end_y = k["endY"]
-        if current_horizontal_y == current_vertical_end_y
-          puts "found match horz vert match. vert = #{k}"
-          context_verticals.push(k)
-        end
-      end
-
-      if context_verticals.length == 0
-
-      end
-      puts "context verticals #{context_verticals} \n"
-      # Not sorting properly
-      sorted_verts = context_verticals.sort_by { |k| k[:startX].to_i }
-      puts "context vertical sorted left to right #{sorted_verts} \n"
-
-      # appending right edge of verticals
-      # this to me does not make sense to add
-      #sorted_verts.push({"clickType" => "vertical", "startX" => @image_width, "startY" => prev_horizontal_y, "endX" => @image_width, "endY" => key["startY"]})
-      puts "sorted after adding: #{sorted_verts}"
-      # context count is for logging
-      context_count = 0
-      previous_vertical_x = 0
-      previous_vertical_y = prev_horizontal_y
-
-      sorted_verts.each do |k, v|
-        # this is a vertical in between other verticals
-        puts "middle block, context_count: #{context_count}, previous verticalx: #{previous_vertical_x} previous vertical_y #{previous_vertical_y}\n"
-        hash = {}
-        hash[:x] = previous_vertical_x
-        hash[:y] = previous_vertical_y
-        hash[:width] = k['startX'].to_i - previous_vertical_x
-        hash[:height] = (k['endY'].to_i - previous_vertical_y).abs #needs to be positive
-        puts "middle block created this hash #{hash} \n"
-        puts "width calculation is #{k[:startX]} - #{previous_vertical_x} \n"
-        puts "which should be equal to #{hash[:width]} \n"
-        previous_vertical_x = k["startX"].to_i
-        previous_vertical_y = k["startY"].to_i
-        @formatted_data.push(hash)
-      end
-
-      prev_horizontal_y = key["startY"].to_i
-    end
   end
 
   # Should use an accessor instead of this

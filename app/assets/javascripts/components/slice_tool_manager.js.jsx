c4d598cb928e84942d3055c08ec07dc9ed5d07b4
joncravotta <joncravotta32@gmail.com>
2016-12-04 13:48:51 -0500

    slice tool talking to code tool

--- app/assets/javascripts/components/slice_tool_manager.js.jsx
+++ app/assets/javascripts/components/slice_tool_manager.js.jsx
@@ -2,14 +2,22 @@ var SliceToolManager = React.createClass({
   getInitialState: function() {
     return {
       managerState: 0,
+      user: this.props.user,
       emailImageFile: "",
       emailImage:"",
       emailHeight:0,
-      emailWidth:0
+      emailWidth:0,
+      returnedUrls: []
     };
   },
 
   updateManagerState: function() {
+    var newState = this.state.managerState + 1;
+    this.setState({managerState: newState});
+  },
+
+  updateManagerStateWithUrls: function(urls) {
+    this.setState({returnedUrls: urls});
     var newState = this.state.managerState + 1;
     this.setState({managerState: newState});
   },
@@ -22,18 +30,20 @@ var SliceToolManager = React.createClass({
       emailHeight: height
     })
   },
+
   handleOrderState: function() {
     switch (this.state.managerState) {
       case 0:
         return (<SliceToolUploader updateUpload={this.propUpdateUpload} updateState={this.updateManagerState}/>);
       case 1:
-          return (<SliceTool file={this.state.emailImageFile} image={this.state.emailImage} imageWidth={this.state.emailWidth} imageHeight={this.state.emailHeight}/>);
+        return (<SliceTool updateState={this.updateManagerStateWithUrls} file={this.state.emailImageFile} image={this.state.emailImage} imageWidth={this.state.emailWidth} imageHeight={this.state.emailHeight}/>);
+      case 2:
+        return (<CodeTool urls={this.state.urls} user={this.state.user} urls={this.state.returnedUrls} emailWidth={this.state.emailWidth}/>);
       default:
         break;
     }
   },
   render: function() {
-    console.log(this.state.emailWidth);
       return (
         <div className="slice-manager-container">
           <SliceToolProgressBar/>
--- app/controllers/slice_tool_controller.rb
+++ app/controllers/slice_tool_controller.rb
@@ -1,5 +1,6 @@
 require_relative "../../lib/slice_model.rb"
 class SliceToolController < ApplicationController
+  before_action :require_user, :require_paid
   respond_to :json
   def index
   end

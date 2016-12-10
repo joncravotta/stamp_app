935080c5f896cfabfb8e4e7b4131804b97db9ba0
joncravotta <joncravotta32@gmail.com>
2016-12-10 11:18:24 -0500

    email name with error validations

--- app/assets/javascripts/components/slice_tool_upload.js.jsx
+++ app/assets/javascripts/components/slice_tool_upload.js.jsx
@@ -5,17 +5,23 @@ var SliceToolUploader = React.createClass({
       image: '',
       emailName: '',
       imageWidth:0,
-      imageHeight:0
+      imageHeight:0,
+      emailNameValid: false,
+      uploadValid: false,
+      showEmailError: false,
+      showUploadError: false
     };
   },
 
-  updateUpload: function() {
-    this.props.updateUpload(this.state.file, this.state.image, this.state.imageWidth, this.state.imageHeight);
-    this.props.updateState();
-  },
-
   handleEmailNameChange: function(event) {
-    this.setState({emailName: event});
+    this.setState({emailName: event.target.value});
+
+    if (event.target.value){
+      this.setState({emailNameValid: true});
+      this.setState({showEmailError: false});
+    } else {
+      this.setState({emailNameValid: false});
+    }
   },
 
    handleFile: function (event) {
@@ -29,20 +35,64 @@ var SliceToolUploader = React.createClass({
       img.onload = function() {
         self.setState({imageWidth: img.width});
         self.setState({imageHeight: img.height});
-    };
-    img.src = reader.result;
+      };
+
+      img.src = reader.result;
+      self.setState({uploadValid: true});
+      self.setState({showUploadError: false});
       self.setState({file: file, image: reader.result});
     };
     reader.readAsDataURL(file);
   },
 
+  handleSubmitButton: function() {
+    if (this.state.emailNameValid == false) {
+      this.setState({showEmailError: true});
+    } else {
+      this.setState({showEmailError: false});
+    }
+
+    if (this.state.uploadValid == false) {
+      this.setState({showUploadError: true});
+    } else {
+      this.setState({showUploadError: false});
+    }
+
+    if (this.state.emailNameValid == true && this.state.uploadValid == true) {
+      console.log('should be valid');
+      this.props.updateUpload(this.state.file, this.state.image, this.state.imageWidth, this.state.imageHeight, this.state.emailName);
+      this.props.updateState();
+    }
+  },
+
+  renderErrorIcon: function() {
+    return (<div className="slice-tool-container-inputs-error">✕</div>);
+  },
+
   render: function() {
+    var showEmailErrorIcon;
+    var showUploadErrorIcon;
+
+    if (this.state.showEmailError == true) {
+      showEmailErrorIcon = this.renderErrorIcon();
+    } else {
+      showEmailErrorIcon = <span></span>;
+    }
+
+    if (this.state.showUploadError == true) {
+      showUploadErrorIcon = this.renderErrorIcon();
+    } else {
+      showEmailErrorIcon = <span></span>;
+    }
+
     return(
-      <div className="slice-tool-container">
+      <div className="slice-tool-container-inputs">
         <div className="slice-component-container">
+          {showEmailErrorIcon}
           <input className="input-lg" placeholder="Email Name" type="text" onChange={this.handleEmailNameChange} />
         </div>
         <div className="slice-component-container">
+          {showUploadErrorIcon}
           <span className="subtitle">Upload your email design</span>
           <span className="subtitle">and lets get slicin'</span>
           <label className="fileContainer">
@@ -50,6 +100,7 @@ var SliceToolUploader = React.createClass({
             <input type="file" onChange={this.handleFile}/>
           </label>
         </div>
+        <div className="dark-button" onClick={this.handleSubmitButton}>Submit</div>
       </div>
     );
   }

547a4ff5922c5ba41aa1131cd6b80cb2385d3333
joncravotta <joncravotta32@gmail.com>
2017-01-04 21:24:26 -0500

    added confirmation to devise and sends email

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161224153723) do
+ActiveRecord::Schema.define(version: 20170104025142) do
 
 # Could not dump table "slices" because of following NoMethodError
 #   undefined method `[]' for nil:NilClass
@@ -51,8 +51,12 @@ ActiveRecord::Schema.define(version: 20161224153723) do
     t.integer  "stripe_canceled_at"
     t.integer  "email_code_count"
     t.string   "subscription_type"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
   end
 
+  add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 

14bfbc05fd9f7803feeafeab488f5b8bdd3048e3
joncravotta <joncravotta32@gmail.com>
2017-01-08 20:37:00 -0500

    auth login with new charge and sub setup

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,25 +1,35 @@
+require 'byebug'
 class ChargesController < ApplicationController
 before_filter :require_user
 def new
 end
 
 def create
+  parameters = sub_params
+
   customer = Stripe::Customer.create(
-    :email => params[:stripeEmail],
-    :source  => params[:stripeToken],
-    :plan => params[:plan_type]
+    :email => parameters[:stripeEmail],
+    :source  => parameters[:stripeToken],
+    :plan => parameters[:stripePlanType]
+  )
+
+
+  charge = Stripe::Subscription.create(
+    :customer => customer.id,
+    :plan => parameters[:stripePlanType],
   )
 
   @user = User.find(current_user.id)
 
   if @user
     puts customer
+    puts charge
     @user.stripe_customer_id = customer.id
-    @user.stripe_current_period_start = customer.subscriptions.data[0].current_period_start
-    @user.stripe_current_period_end = customer.subscriptions.data[0].current_period_end
-    @user.stripe_plan_id = customer.subscriptions.data[0].id
-    @user.email_code_count = email_count(params[:plan_type])
-    @user.subscription_type = params[:plan_type]
+    @user.stripe_current_period_start = charge.current_period_start
+    @user.stripe_current_period_end = charge.current_period_end
+    @user.stripe_plan_id = charge.id
+    @user.email_code_count = email_count(charge.plan.id)
+    @user.subscription_type = charge.plan.id
     @user.paid = true
     @user.save
   else
@@ -31,5 +41,11 @@ rescue Stripe::CardError => e
   flash[:error] = e.message
   redirect_to new_charge_path
 end
-  
+
+  private
+
+  def sub_params
+    params.permit(:stripeEmail, :stripeToken, :stripePlanType)
+  end
+
 end

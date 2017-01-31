442df40500ea18f1d89936280075cc5f14679ebb
joncravotta <joncravotta32@gmail.com>
2017-01-30 20:18:11 -0500

    added dev and prod to stripe config

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,6 +1,15 @@
-Rails.configuration.stripe = {
-  :publishable_key => ENV['PUBLISHABLE_KEY'],
-  :secret_key      => ENV['SECRET_KEY']
-}
+if Rails.env.production?
+  Rails.configuration.stripe = {
+    :publishable_key => ENV['PUBLISHABLE_KEY'],
+    :secret_key      => ENV['SECRET_KEY']
+  }
 
-Stripe.api_key = Rails.configuration.stripe[:secret_key]
+  Stripe.api_key = Rails.configuration.stripe[:secret_key]
+else
+  Rails.configuration.stripe = {
+    :publishable_key => ENV['STRIPE_DEV_PUBLISHABLE_KEY'],
+    :secret_key      => ENV['STRIPE_DEV_SECRET_KEY']
+  }
+
+  Stripe.api_key = Rails.configuration.stripe[:secret_key]
+end

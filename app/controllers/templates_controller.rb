db8287b5a81b6f5717a0fb0403c5888554b0ded6
joncravotta <joncravotta32@gmail.com>
2017-02-04 20:54:47 -0500

    templates show page

--- app/controllers/templates_controller.rb
+++ app/controllers/templates_controller.rb
@@ -6,5 +6,7 @@ class TemplatesController < ApplicationController
 
   def show
     @template = Template.find(params[:id])
+    @images = UploadedImage.where(template_id: params[:id])
+    @created_by = User.find(@template.user_id)
   end
 end

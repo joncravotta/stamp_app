c103349aab9edda46d6d43b3cdcdc16e3a2e093f
joncravotta <joncravotta32@gmail.com>
2017-02-07 19:25:09 -0500

    profile ui update

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -2,19 +2,5 @@ class ProfileController < ApplicationController
   def index
     @profile = User.find(current_user)
     @account = Account.find(@profile.account_id)
-    @sub_type = sub_type(@account.stripe_sub_type)
-  end
-
-  private
-
-  def sub_type(sub)
-    case sub
-    when 'SUB_199'
-      '50'
-    when 'SUB_299'
-      '125'
-    when 'SUB_399'
-      'unlimited'
-    end
   end
 end
